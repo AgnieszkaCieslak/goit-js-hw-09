fd7a80de8ca26cc40e304fbd8efacb1041c3d49d
AgnieszkaCieslak <114153531+AgnieszkaCieslak@users.noreply.github.com>
2023-10-06 08:47:03 +0000

    Deploying to gh-pages from @ AgnieszkaCieslak/goit-js-hw-09@82f0301c7b277650ab448e86640a0cdbd8b0d80b 🚀

---
+++ 01-color-switcher.e994c014.js
@@ -0,0 +1,2 @@
+let t=null,n=!1;const o={startButton:document.querySelector("button[data-start]"),stopButton:document.querySelector("button[data-stop]")};function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}o.startButton.addEventListener("click",function(){n||(n=!0,o.startButton.disabled=!0,e(),t=setInterval(e,1e3))}),o.stopButton.addEventListener("click",function(){n&&(n=!1,clearInterval(t),o.startButton.disabled=!1)});
+//# sourceMappingURL=01-color-switcher.e994c014.js.map
